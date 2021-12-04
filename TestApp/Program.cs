3fec0e36dc54cedeacf920e7139e1bd68d3fd62a
Nikita Tarasov <77894178+dev-nikitat@users.noreply.github.com>
2021-12-04 19:41:03 +0200

    some methods

---
+++ TestApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            DateTime birthday;
+            Console.Write("Enter Birthday: ");
+           birthday = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine($"Welcome  {name}!");
+        }
+    }
+}
